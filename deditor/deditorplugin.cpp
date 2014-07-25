092213bd6cd012649e25435007f3e897487ab272
GoldMax <goldmax3000@gmail.com>
2014-07-25 17:52:49 +0700

    .

--- deditor/deditorplugin.cpp
+++ deditor/deditorplugin.cpp
@@ -38,137 +38,134 @@ using namespace DEditor::Internal;
 DEditorPlugin *DEditorPlugin::m_instance = 0;
 
 DEditorPlugin::DEditorPlugin()
- : m_editorFactory(0),
-   m_settings(0)
+	: m_editorFactory(0),
+			m_settings(0)
 {
- QTC_ASSERT(!m_instance, return);
- m_instance = this;
+	QTC_ASSERT(!m_instance, return);
+	m_instance = this;
 }
 DEditorPlugin::~DEditorPlugin()
 {
- removeObject(m_editorFactory);
- removeObject(m_settings);
+	removeObject(m_editorFactory);
+	removeObject(m_settings);
 
- //delete m_actionHandler;
- m_instance = 0;
+	//delete m_actionHandler;
+	m_instance = 0;
 }
 
 bool DEditorPlugin::initialize(const QStringList &arguments, QString *errorString)
 {
- // Register objects in the plugin manager's object pool
- // Load settings
- // Add actions to menus
- // Connect to other plugins' signals
- // In the initialize method, a plugin can be sure that the plugins it
- // depends on have initialized their members.
+	// Register objects in the plugin manager's object pool
+	// Load settings
+	// Add actions to menus
+	// Connect to other plugins' signals
+	// In the initialize method, a plugin can be sure that the plugins it
+	// depends on have initialized their members.
 
- Q_UNUSED(arguments)
+	Q_UNUSED(arguments)
 
- if (!MimeDatabase::addMimeTypes(QLatin1String(":/deditor/DEditor.mimetypes.xml"), errorString))
-  return false;
- FileIconProvider::registerIconOverlayForMimeType(":/deditor/images/d.png", Constants::D_MIMETYPE_SRC);
- FileIconProvider::registerIconOverlayForMimeType(":/deditor/images/di.png", Constants::D_MIMETYPE_HDR);
+	if (!MimeDatabase::addMimeTypes(QLatin1String(":/deditor/DEditor.mimetypes.xml"), errorString))
+		return false;
+	FileIconProvider::registerIconOverlayForMimeType(":/deditor/images/d.png", Constants::D_MIMETYPE_SRC);
+	FileIconProvider::registerIconOverlayForMimeType(":/deditor/images/di.png", Constants::D_MIMETYPE_HDR);
 
- m_settings = TextEditorSettings::instance();
- if(!m_settings)
-  m_settings = new TextEditorSettings(this);
+	m_settings = TextEditorSettings::instance();
+	if(!m_settings)
+		m_settings = new TextEditorSettings(this);
 
- m_editorFactory = new DEditorFactory(this);
- addObject(m_editorFactory);
+	m_editorFactory = new DEditorFactory(this);
+	addObject(m_editorFactory);
 
- addAutoReleasedObject(new DCompletionAssistProvider);
- addAutoReleasedObject(new DHoverHandler(this));
+	addAutoReleasedObject(new DCompletionAssistProvider);
+	addAutoReleasedObject(new DHoverHandler(this));
 	addAutoReleasedObject(new DEditorHighlighterFactory);
 
- //QObject *core = ICore::instance();
- DFileWizard* wizard = new DFileWizard(DFileWizard::Source);
- wizard->setWizardKind(IWizard::FileWizard);
- wizard->setCategory(QLatin1String(Constants::WIZARD_CATEGORY_D));
- wizard->setDisplayCategory(QCoreApplication::translate("DEditor", Constants::WIZARD_TR_CATEGORY_D));
- wizard->setDescription(tr("Creates a D source file."));
- wizard->setDisplayName(tr("D Source File"));
- wizard->setId(QLatin1String("A.Source"));
- addAutoReleasedObject(wizard);
+	//QObject *core = ICore::instance();
+	DFileWizard* wizard = new DFileWizard(DFileWizard::Source);
+	wizard->setWizardKind(IWizard::FileWizard);
+	wizard->setCategory(QLatin1String(Constants::WIZARD_CATEGORY_D));
+	wizard->setDisplayCategory(QCoreApplication::translate("DEditor", Constants::WIZARD_TR_CATEGORY_D));
+	wizard->setDescription(tr("Creates a D source file."));
+	wizard->setDisplayName(tr("D Source File"));
+	wizard->setId(QLatin1String("A.Source"));
+	addAutoReleasedObject(wizard);
 
- wizard = new DFileWizard(DFileWizard::Header);
- wizard->setWizardKind(IWizard::FileWizard);
- wizard->setCategory(QLatin1String(Constants::WIZARD_CATEGORY_D));
- wizard->setDisplayCategory(QCoreApplication::translate("DEditor", Constants::WIZARD_TR_CATEGORY_D));
- wizard->setDescription(tr("Creates a D header file."));
- wizard->setDisplayName(tr("D Header File"));
- wizard->setId(QLatin1String("B.Header"));
- addAutoReleasedObject(wizard);
+	wizard = new DFileWizard(DFileWizard::Header);
+	wizard->setWizardKind(IWizard::FileWizard);
+	wizard->setCategory(QLatin1String(Constants::WIZARD_CATEGORY_D));
+	wizard->setDisplayCategory(QCoreApplication::translate("DEditor", Constants::WIZARD_TR_CATEGORY_D));
+	wizard->setDescription(tr("Creates a D header file."));
+	wizard->setDisplayName(tr("D Header File"));
+	wizard->setId(QLatin1String("B.Header"));
+	addAutoReleasedObject(wizard);
 
- QAction *action;
- Core::Command *cmd;
+	QAction *action;
+	Core::Command *cmd;
 
- //*** Tools submenu *****************
- Core::ActionContainer *menu = Core::ActionManager::createMenu(Constants::M_TOOLS_D);
- menu->menu()->setTitle(tr("D"));
- //-- Enable/disable DCD
- action = new QAction(tr("Enable DCD"), this);
- cmd = Core::ActionManager::registerAction(action, Constants::D_ACTION_TOGGLE_DCD_ID,
-                                           Core::Context(Core::Constants::C_GLOBAL));
- action->setCheckable(true);
- action->setChecked(true);
- connect(action, SIGNAL(triggered()), this, SLOT(toggleDCDAction()));
- menu->addAction(cmd);
- //-- Clear code assist cache
- action = new QAction(tr("Clear DCD cache"), this);
- cmd = Core::ActionManager::registerAction(action, Constants::D_ACTION_CLEARASSISTCACHE_ID,
-                                           Core::Context(Core::Constants::C_GLOBAL));
- connect(action, SIGNAL(triggered()), this, SLOT(clearAssistCacheAction()));
- menu->addAction(cmd);
- //--
- Core::ActionManager::actionContainer(Core::Constants::M_TOOLS)->addMenu(menu);
- //*** Tools submenu *****************
+	//*** Tools submenu *****************
+	Core::ActionContainer *menu = Core::ActionManager::createMenu(Constants::M_TOOLS_D);
+	menu->menu()->setTitle(tr("D"));
+	//-- Enable/disable DCD
+	action = new QAction(tr("Enable DCD"), this);
+	cmd = Core::ActionManager::registerAction(action, Constants::D_ACTION_TOGGLE_DCD_ID,
+																																											Core::Context(Core::Constants::C_GLOBAL));
+	action->setCheckable(true);
+	action->setChecked(true);
+	connect(action, SIGNAL(triggered()), this, SLOT(toggleDCDAction()));
+	menu->addAction(cmd);
+	//-- Clear code assist cache
+	action = new QAction(tr("Clear DCD cache"), this);
+	cmd = Core::ActionManager::registerAction(action, Constants::D_ACTION_CLEARASSISTCACHE_ID,
+																																											Core::Context(Core::Constants::C_GLOBAL));
+	connect(action, SIGNAL(triggered()), this, SLOT(clearAssistCacheAction()));
+	menu->addAction(cmd);
+	//--
+	Core::ActionManager::actionContainer(Core::Constants::M_TOOLS)->addMenu(menu);
+	//*** Tools submenu *****************
 
- //Context context(Constants::C_DEDITOR_ID);
- ActionContainer *contextMenu =
-   ActionManager::createMenu(Constants::M_CONTEXT);
+	//Context context(Constants::C_DEDITOR_ID);
+	ActionContainer *contextMenu =
+			ActionManager::createMenu(Constants::M_CONTEXT);
 
- //contextMenu->addSeparator(context);
- cmd = ActionManager::command(TextEditor::Constants::AUTO_INDENT_SELECTION);
- contextMenu->addAction(cmd);
- cmd = ActionManager::command(TextEditor::Constants::UN_COMMENT_SELECTION);
- contextMenu->addAction(cmd);
- //***
+	//contextMenu->addSeparator(context);
+	cmd = ActionManager::command(TextEditor::Constants::AUTO_INDENT_SELECTION);
+	contextMenu->addAction(cmd);
+	cmd = ActionManager::command(TextEditor::Constants::UN_COMMENT_SELECTION);
+	contextMenu->addAction(cmd);
+	//***
 
- errorString->clear();
- return true;
+	errorString->clear();
+	return true;
 }
 
 void DEditorPlugin::toggleDCDAction()
 {
- QcdAssist::isDCDEnabled(!QcdAssist::isDCDEnabled());
+	QcdAssist::isDCDEnabled(!QcdAssist::isDCDEnabled());
 }
 void DEditorPlugin::clearAssistCacheAction()
 {
- QcdAssist::sendClearChache();
+	QcdAssist::sendClearChache();
 }
 
 void DEditorPlugin::extensionsInitialized()
 {
- // Retrieve objects from the plugin manager's object pool
- // In the extensionsInitialized method, a plugin can be sure that all
- // plugins that depend on it are completely initialized.
+	// Retrieve objects from the plugin manager's object pool
+	// In the extensionsInitialized method, a plugin can be sure that all
+	// plugins that depend on it are completely initialized.
 }
 
 ExtensionSystem::IPlugin::ShutdownFlag DEditorPlugin::aboutToShutdown()
 {
- // Save settings
- // Disconnect from signals that are not needed during shutdown
- // Hide UI (if you add UI that is not in the main window directly)
+	// Save settings
+	// Disconnect from signals that are not needed during shutdown
+	// Hide UI (if you add UI that is not in the main window directly)
 
- disconnect(m_settings, SIGNAL(fontSettingsChanged(TextEditor::FontSettings)),
-         this, SLOT(updateSearchResultsFont(TextEditor::FontSettings)));
-
- return SynchronousShutdown;
+	return SynchronousShutdown;
 }
 
 void DEditorPlugin::initializeEditor(DTextEditorWidget* editor)
 {
- TextEditorSettings::initializeEditor(editor);
+	TextEditorSettings::initializeEditor(editor);
 }
 
 Q_EXPORT_PLUGIN2(DEditor, DEditorPlugin)
